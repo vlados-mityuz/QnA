a8e8d19a6499d5ed63327e82dbc62e930a3d9611
Vladislav <thenoma1@gmail.com>
2022-01-29 11:01:50 +0300

    Answer model with specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_200619) do
+ActiveRecord::Schema.define(version: 2022_01_27_145628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "answer_id", null: false
+    t.index ["answer_id"], name: "index_questions_on_answer_id"
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "answers"
 end
