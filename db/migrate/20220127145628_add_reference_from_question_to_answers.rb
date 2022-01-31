926e84c8a98ca47a94d506268ea20a40f2d1e5c2
Vladislav <thenoma1@gmail.com>
2022-01-31 23:54:43 +0300

    Created Questions controller

--- db/migrate/20220127145628_add_reference_from_question_to_answers.rb
+++ db/migrate/20220127145628_add_reference_from_question_to_answers.rb
@@ -1,5 +1,5 @@
 class AddReferenceFromQuestionToAnswers < ActiveRecord::Migration[7.0]
   def change
-    add_reference :questions, :answer, foreign_key: true, null: false, on_delete: :cascade
+    add_reference :questions, :answer, foreign_key: true, on_delete: :cascade
   end
 end
