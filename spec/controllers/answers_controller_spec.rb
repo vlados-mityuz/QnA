05f828d055dde537b725acd6446ff84e37a03af2
Vladislav <thenoma1@gmail.com>
2022-02-02 11:45:54 +0300

    Update saving a new answer test in Answers controller tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attrubutes' do
-      it 'saves a new question in the database' do
-        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      it 'saves a new answer in the database' do
+        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
       end
 
       it 'redirects to question view' do
